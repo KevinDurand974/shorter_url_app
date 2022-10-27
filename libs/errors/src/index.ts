09fd7bec51682984f3aa577039d36a987bb9542a
KDurand974 <k.durand.974@gmail.com>
2022-10-27 14:08:10 +0200

    Add Errors lib

---
+++ libs/errors/src/index.ts
@@ -0,0 +1,27 @@
+import { ZodIssue } from "zod";
+import createError from "http-errors";
+import { parseZodError } from "@shorter/validators";
+
+export const createValidationError = (zodErrors: ZodIssue[]) => {
+	return createError(
+		400,
+		"Error when validating data",
+		parseZodError(zodErrors)
+	);
+};
+
+export const createError404 = (message: string) => {
+	return createError(404, message);
+};
+
+export const createError403 = (message: string) => {
+	return createError(403, message);
+};
+
+export const createError401 = (message: string) => {
+	return createError(401, message);
+};
+
+export const createError400 = (message: string) => {
+	return createError(400, message);
+};
