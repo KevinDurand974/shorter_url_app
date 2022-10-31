64d0e84473de26d761b8bb1ffcb4111e77d8d12d
KDurand974 <k.durand.974@gmail.com>
2022-10-31 20:49:18 +0100

    Change most of custom error from HttpError to TRPCError

--- libs/errors/src/index.ts
+++ libs/errors/src/index.ts
@@ -1,5 +1,6 @@
 import { ZodIssue } from "zod";
 import createError from "http-errors";
+import { TRPCError } from "@trpc/server";
 import { parseZodError } from "@shorter/validators";
 
 export const createValidationError = (zodErrors: ZodIssue[]) => {
@@ -9,17 +10,29 @@ export const createValidationError = (zodErrors: ZodIssue[]) => {
 };
 
 export const createError404 = (message: string) => {
-	return createError(404, message);
+	return new TRPCError({
+		code: "NOT_FOUND",
+		message,
+	});
 };
 
 export const createError403 = (message: string) => {
-	return createError(403, message);
+	return new TRPCError({
+		code: "FORBIDDEN",
+		message,
+	});
 };
 
 export const createError401 = (message: string) => {
-	return createError(401, message);
+	return new TRPCError({
+		code: "UNAUTHORIZED",
+		message,
+	});
 };
 
 export const createError400 = (message: string) => {
-	return createError(400, message);
+	return new TRPCError({
+		code: "BAD_REQUEST",
+		message,
+	});
 };
