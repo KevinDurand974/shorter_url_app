5f644021f637256146eb70774e439dc8cd7b0846
KDurand974 <k.durand.974@gmail.com>
2022-11-01 18:35:16 +0100

    Update Profile and remove ShortUrl Entities

--- apps/backend/src/entities/Profile.ts
+++ apps/backend/src/entities/Profile.ts
@@ -1,68 +1,73 @@
 import {
-	Column,
-	CreateDateColumn,
-	Entity,
-	Generated,
-	Index,
-	JoinColumn,
-	OneToMany,
-	OneToOne,
-	PrimaryGeneratedColumn,
-	UpdateDateColumn,
-} from "typeorm";
-import type { Relation } from "typeorm";
-import { ShortUrl } from "./ShortUrl";
-import { User } from "./User";
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  Index,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import type { Relation } from 'typeorm';
+import { Url } from './Url';
+import { User } from './User';
 
-@Entity()
+@Entity('profiles')
 export class Profile {
-	@PrimaryGeneratedColumn()
-	id!: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-	@Index()
-	@Generated("uuid")
-	@Column({ type: "uuid", unique: true })
-	uuid!: string;
+  // NOTE: Profile UUID
+  @Index()
+  @Generated('uuid')
+  @Column({ type: 'uuid', unique: true })
+  uuid!: string;
 
-	@Column({ type: "boolean", default: false })
-	verified!: boolean;
+  // NOTE: is Email verified
+  @Column({ type: 'boolean', default: false })
+  verified!: boolean;
 
-	@Index()
-	@Column({
-		length: 20,
-		type: "character varying",
-		nullable: true,
-	})
-	urlName!: string;
+  // NOTE: Url Name for generated Urls
+  @Index()
+  @Column({
+    length: 20,
+    type: 'character varying',
+    nullable: true,
+  })
+  urlName!: string;
 
-	@Column({ type: "boolean", default: false })
-	urlActive!: boolean;
+  // NOTE: If the user is VIP
+  @Column({ type: 'boolean', default: false })
+  vip!: boolean;
 
-	@Column({ type: "boolean", default: false })
-	vip!: boolean;
+  // NOTE: Maximum number of urls the user can create
+  @Column({ type: 'integer', default: 25 })
+  maxUrls!: number;
 
-	@Column({ type: "integer", default: 25 })
-	maxUrls!: number;
+  // NOTE: Available number of urls the user can create
+  @Column({ type: 'integer', default: 25 })
+  availableUrls!: number;
 
-	@Column({ type: "integer", default: 25 })
-	availableUrls!: number;
+  // NOTE: Profile creation date
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt!: Date;
 
-	@CreateDateColumn({ type: "timestamptz" })
-	createdAt!: Date;
+  // NOTE: Profile update date
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt!: Date;
 
-	@UpdateDateColumn({ type: "timestamptz" })
-	updatedAt!: Date;
+  @OneToOne(() => User, (u) => u.profile, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  user!: Relation<User>;
 
-	@OneToOne(() => User, (u) => u.profile, {
-		cascade: true,
-		onDelete: "CASCADE",
-	})
-	@JoinColumn()
-	user!: Relation<User>;
-
-	@OneToMany(() => ShortUrl, (s) => s.profile, {
-		cascade: true,
-		onDelete: "CASCADE",
-	})
-	shortUrls!: Relation<ShortUrl[]>;
+  @OneToMany(() => Url, (s) => s.profile, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  urls!: Relation<Url[]>;
 }
