a3e500892b85ccf1750a7d37202257bf20566115
KDurand974 <k.durand.974@gmail.com>
2022-11-01 18:47:17 +0100

    Add base code in file

--- apps/backend/src/libs/trpc/procedures/user.ts
+++ apps/backend/src/libs/trpc/procedures/user.ts
@@ -1,1 +1,10 @@
-export {};
+import { getDataSource } from '@libs/typeorm';
+
+import { publicProcedure, router } from '../configuration';
+
+export const userRouter = router({
+  createUser: publicProcedure.input().mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+});
