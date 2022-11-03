4ed0bcf4af3f6b3962c48c0a65f07454d39195cf
KDurand974 <k.durand.974@gmail.com>
2022-11-03 17:20:54 +0100

    Add user procedures router to global router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -9,7 +9,7 @@ import { createExpressMiddleware } from '@trpc/server/adapters/express';
 
 import { getDataSource } from '@libs/typeorm';
 import { createContext, mergeRouters } from '@libs/trpc';
-import { urlRouter } from '@libs/trpc/procedures';
+import { urlRouter, userRouter } from '@libs/trpc/procedures';
 
 type JsonErrorResult = {
   status: number;
@@ -23,7 +23,7 @@ type JsonErrorResult = {
 };
 
 // CONFIG TRPC HERE
-const appRouter = mergeRouters(urlRouter);
+const appRouter = mergeRouters(urlRouter, userRouter);
 
 const app = express();
 
