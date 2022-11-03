74f82bdf001231eff6a738f8fea1651e05fbcd98
KDurand974 <k.durand.974@gmail.com>
2022-11-03 15:31:24 +0100

    Update and prepare for user models

--- apps/backend/src/libs/trpc/procedures/user.ts
+++ apps/backend/src/libs/trpc/procedures/user.ts
@@ -1,9 +1,51 @@
 import { getDataSource } from '@libs/typeorm';
+import {
+  createUserSchema,
+  deleteUserSchema,
+  getUserSchema,
+  updateUserEmailSchema,
+  updateUserPasswordSchema,
+  updateUserPseudoSchema,
+  updateUserUrlNameSchema,
+  updateUserVIPSchema,
+} from '@shorter/validators';
 
 import { publicProcedure, router } from '../configuration';
 
 export const userRouter = router({
-  createUser: publicProcedure.input().mutation(async ({ input, ctx }) => {
+  createUser: publicProcedure.input(createUserSchema).mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  deleteUser: publicProcedure.input(deleteUserSchema).mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  getUser: publicProcedure.input(getUserSchema).query(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  getUsers: publicProcedure.query(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  updateUserEmail: publicProcedure.input(updateUserEmailSchema).mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  updateUserPassword: publicProcedure.input(updateUserPasswordSchema).mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  updateUserVIp: publicProcedure.input(updateUserVIPSchema).mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  updateUserPseudo: publicProcedure.input(updateUserPseudoSchema).mutation(async ({ input, ctx }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  updateUserUrlName: publicProcedure.input(updateUserUrlNameSchema).mutation(async ({ input, ctx }) => {
     const datasource = await getDataSource();
     return true;
   }),
