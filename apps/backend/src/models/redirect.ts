f9c3d285090d83fa14e725df8012f518da96276e
KDurand974 <k.durand.974@gmail.com>
2022-11-09 15:04:39 +0100

    Reset function and start from zero

--- apps/backend/src/models/redirect.ts
+++ apps/backend/src/models/redirect.ts
@@ -1,45 +1,8 @@
 import { DataSource } from 'typeorm';
-import { add, isAfter } from 'date-fns';
-import { Url } from '@entities';
-import { createValidationError } from '@shorter/errors';
-import { GetShortUrlSchema, validateGetShortUrl } from '@shorter/validators';
+import { GetRedirectUrlSchema } from '@shorter/validators';
 
-export const getShortUrl = async (datasource: DataSource, data: GetShortUrlSchema) => {
+export const getRedirectUrl = async (datasource: DataSource, data: GetRedirectUrlSchema) => {
   try {
-    // Validating Data
-    const validatingData = await validateGetShortUrl(data);
-    if (!validatingData.success) {
-      throw createValidationError(validatingData.error.issues);
-    }
-    const validatedData = validatingData.data;
-
-    // Check if the short url exist
-    const ShortUrlRep = datasource.getRepository(Url);
-    const shortUrl = await ShortUrlRep.findOneBy({
-      generatedUrl: validatedData.url,
-    });
-
-    // Cancel if the short url doesn't exist
-    if (!shortUrl) return null;
-
-    // Check if the short url is ephemeral
-    if (shortUrl.ephemeral) {
-      // Check if the short url is expired
-      const maxDate = add(new Date(shortUrl.createdAt), {
-        seconds: +shortUrl.duration,
-      });
-      const now = new Date();
-      if (isAfter(now, maxDate)) return null;
-    }
-
-    // Increment the number of clicks
-    shortUrl.useCount = +shortUrl.useCount + 1;
-
-    // Save the short url
-    await ShortUrlRep.save(shortUrl);
-
-    // Return the redirect url
-    return shortUrl.redirect;
   } catch (err) {
     throw err;
   }
