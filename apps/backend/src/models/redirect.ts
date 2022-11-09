48cb8448095227abc4c4644ead35d57f81d1052c
KDurand974 <k.durand.974@gmail.com>
2022-11-09 15:45:57 +0100

    Update redirect procedure

--- apps/backend/src/models/redirect.ts
+++ apps/backend/src/models/redirect.ts
@@ -1,8 +1,32 @@
+import { addSeconds, isAfter } from 'date-fns';
 import { DataSource } from 'typeorm';
+
 import { GetRedirectUrlSchema } from '@shorter/validators';
+import { createError400, createError404 } from '@shorter/errors';
+import { Url } from '@entities';
 
 export const getRedirectUrl = async (datasource: DataSource, data: GetRedirectUrlSchema) => {
   try {
+    // Get url from database
+    const UrlRep = datasource.getRepository(Url);
+    const url = await UrlRep.findOne({ where: { generatedUrl: data.path } });
+    if (!url) throw createError404('Url not found');
+
+    // If restricted
+    if (url.restricted) throw createError400("Cannot use this url because it's restricted");
+
+    // If disabled
+    if (!url.enabled) throw createError400("Cannot use this url because it's disabled by the owner");
+
+    // If the url is ephemeral, check if it has expired
+    if (url.ephemeral) {
+      const maxDate = addSeconds(url.updatedAt, url.duration);
+      const now = new Date();
+      if (isAfter(now, maxDate)) throw createError400('This url has expired');
+    }
+
+    // Return the redirect url
+    return url.redirect;
   } catch (err) {
     throw err;
   }
