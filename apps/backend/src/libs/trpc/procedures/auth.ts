19329f807a8230b08531f5d210c0b445a7ae499b
KDurand974 <k.durand.974@gmail.com>
2022-11-09 17:25:24 +0100

    Add validators to procedure and model

--- apps/backend/src/libs/trpc/procedures/auth.ts
+++ apps/backend/src/libs/trpc/procedures/auth.ts
@@ -1,20 +1,17 @@
+import { loginSchema } from '@shorter/validators';
 import { getDataSource } from '@libs/typeorm';
 
 import { publicProcedure, router } from '../configuration';
 
 // FIX: Use authProcedure to get UUID
 export const authRouter = router({
-  login: publicProcedure
-    .input((a) => a)
-    .mutation(async ({ input }) => {
-      const datasource = await getDataSource();
-      return true;
-    }),
-  logout: publicProcedure
-    .input((a) => a)
-    .mutation(async ({ input }) => {
-      const datasource = await getDataSource();
-      return true;
-    }),
+  login: publicProcedure.input(loginSchema).mutation(async ({ input }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
+  logout: publicProcedure.mutation(async ({ input }) => {
+    const datasource = await getDataSource();
+    return true;
+  }),
   // TODO: Forgot Password ?
 });
--- apps/backend/src/models/auth.ts
+++ apps/backend/src/models/auth.ts
@@ -1,15 +1,19 @@
 import { DataSource } from 'typeorm';
 import { createError400, createError404 } from '@shorter/errors';
+import { LoginSchema } from '@shorter/validators';
 import { Profile, User } from '@entities';
 
-export const login = async (datasource: DataSource, data: any) => {
+type Uuid = { uuid: string };
+type LoginInput = LoginSchema & Uuid;
+
+export const login = async (datasource: DataSource, data: LoginInput) => {
   try {
   } catch (err) {
     throw err;
   }
 };
 
-export const logout = async (datasource: DataSource, data: any) => {
+export const logout = async (datasource: DataSource, data: Uuid) => {
   try {
   } catch (err) {
     throw err;
