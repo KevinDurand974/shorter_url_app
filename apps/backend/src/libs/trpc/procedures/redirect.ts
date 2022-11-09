28d9d32079f6847b3134d6598e0f0d62584213db
KDurand974 <k.durand.974@gmail.com>
2022-11-09 15:06:49 +0100

    Update redirect procedure

--- apps/backend/src/libs/trpc/procedures/redirect.ts
+++ apps/backend/src/libs/trpc/procedures/redirect.ts
@@ -1,12 +1,12 @@
 import { getDataSource } from '@libs/typeorm';
+import { getRedirectUrl } from '@models';
+import { getRedirectUrlSchema } from '@shorter/validators';
 
 import { publicProcedure, router } from '../configuration';
 
 export const redirectRouter = router({
-  redirect: publicProcedure
-    .input((a) => a)
-    .mutation(async ({ input, ctx }) => {
-      const datasource = await getDataSource();
-      return true;
-    }),
+  redirect: publicProcedure.input(getRedirectUrlSchema).mutation(async ({ input }) => {
+    const datasource = await getDataSource();
+    return getRedirectUrl(datasource, input);
+  }),
 });
