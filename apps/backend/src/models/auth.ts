bb20583472f12cedc0c7f460902c84e3b924b724
KDurand974 <k.durand.974@gmail.com>
2022-11-09 17:15:23 +0100

    Add auth model setup

---
+++ apps/backend/src/models/auth.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+import { createError400, createError404 } from '@shorter/errors';
+import { Profile, User } from '@entities';
+
+export const login = async (datasource: DataSource, data: any) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const logout = async (datasource: DataSource, data: any) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const register = async (datasource: DataSource, data: any) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
--- apps/backend/src/models/index.ts
+++ apps/backend/src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './user';
 export * from './url';
 export * from './redirect';
+export * from './auth';
