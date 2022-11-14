c8119a665a5bf608ea5d944b06654a7a44bdc2e8
KDurand974 <k.durand.974@gmail.com>
2022-11-14 22:02:16 +0100

    Add verify token helper

--- apps/backend/src/helpers/token.ts
+++ apps/backend/src/helpers/token.ts
@@ -1,6 +1,11 @@
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
-type Payload = string | object | Buffer;
+type Payload = {
+  uuid: string;
+  vip: boolean;
+  pseudo: string;
+  emailChecked: boolean;
+};
 
 export const createAccessToken = (payload: Payload) => {
   return sign(payload, process.env.ACCESS_TOKEN_SECRET!, {
@@ -12,4 +17,11 @@ export const createRefreshToken = (payload: Payload) => {
   return sign(payload, process.env.REFRESH_TOKEN_SECRET!, {
     expiresIn: '30d',
   });
+};
+
+export const verifyToken = (token: string, type: 'access' | 'refresh') => {
+  return verify(
+    token,
+    type === 'access' ? process.env.ACCESS_TOKEN_SECRET! : process.env.REFRESH_TOKEN_SECRET!
+  ) as Payload;
 };
