65871ec61f5b57a747c81893cb9c6b17eea97e8f
KDurand974 <k.durand.974@gmail.com>
2022-11-14 22:05:55 +0100

    Update authRouter

--- apps/backend/src/libs/trpc/procedures/auth.ts
+++ apps/backend/src/libs/trpc/procedures/auth.ts
@@ -2,16 +2,25 @@ import { loginSchema } from '@shorter/validators';
 import { getDataSource } from '@libs/typeorm';
 
 import { publicProcedure, router } from '../configuration';
+import { auth, login, logout, refreshToken } from '@models';
 
 // FIX: Use authProcedure to get UUID
 export const authRouter = router({
-  login: publicProcedure.input(loginSchema).mutation(async ({ input }) => {
+  login: publicProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
     const datasource = await getDataSource();
-    return true;
+    return login(datasource, input, ctx);
   }),
-  logout: publicProcedure.mutation(async ({ input }) => {
+  logout: publicProcedure.mutation(async ({ ctx }) => {
     const datasource = await getDataSource();
-    return true;
+    await logout(datasource, ctx);
+  }),
+  refresh: publicProcedure.mutation(async ({ ctx }) => {
+    const datasource = await getDataSource();
+    return refreshToken(datasource, ctx);
+  }),
+  me: publicProcedure.mutation(async ({ ctx }) => {
+    const datasource = await getDataSource();
+    return auth(datasource, ctx);
   }),
   // TODO: Forgot Password ?
 });
