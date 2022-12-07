50f88e55377f31687309e6fb7dd6aea9ce5188df
Kdurand974 <k.durand.974@gmail.com>
2022-12-07 19:20:23 +0100

    Move public folder to root, update tsconfig, add favicon and continue style on footer

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="w-full flex items-center bg-gradient-to-b from-black/10 to-black/30 p-4 flex-shrink-0 rounded-xl shadow-md shadow-t-alt/40 justify-evenly flex-wrap">
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         {/* FIX: Image */}
-        <Image src="" alt="" />
-        <h4 className="font-fredoka caps-small text-xl">Url Shorten</h4>
+        <div className="relative h-10 w-10">
+          <Image src="/icon.png" alt="" layout="fill" />
+        </div>
+        <h4 className="font-fredoka caps-small text-3xl">Url Shorten</h4>
       </div>
-      <div>
-        <p>2022 KDWeb v1.0 - Sous licence MIT</p>
-        <p>Pour signaler des potentiels bugs, rendez-vous ici</p>
-      </div>
+      <ul className="list-disc">
+        <li>2022 - KDWeb v1.0 - Sous licence MIT</li>
+        <li>Pour signaler des potentiels bugs, rendez-vous ici</li>
+      </ul>
     </footer>
   );
 }
