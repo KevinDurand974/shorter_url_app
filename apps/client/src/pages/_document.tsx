687de42191e471de70be7b35a989a32dbeb37391
Kdurand974 <k.durand.974@gmail.com>
2022-12-07 19:33:08 +0100

    Update favicons and move global meta tags to _document file

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -2,8 +2,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
 	return (
-		<Html>
+		<Html lang="en">
 			<Head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				{/* <base href="//cdn.example.com/" /> */}
+				{/* <meta name="application-name" content="Application Name" /> */}
+
+				<link rel="icon" href="/favicon-16.png" sizes="16x16" type="image/png" />
+				<link rel="icon" href="/favicon-32.png" sizes="32x32" type="image/png" />
+				<link rel="icon" href="/favicon-96.png" sizes="96x96" type="image/png" />
+				<link rel="icon" href="/favicon-192.png" sizes="192x192" type="image/png" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Fredoka+One&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
