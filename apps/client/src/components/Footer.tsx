ff0d01e96f160894f9a233472e8821b024b8d91f
Kdurand974 <k.durand.974@gmail.com>
2022-12-07 20:44:29 +0100

    Update logo on footer and header

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -1,21 +1,27 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
+import { Logo } from "./svg"
 
 const Footer = () => {
-  return (
-    <footer className="w-full flex items-center bg-gradient-to-b from-black/10 to-black/30 p-4 flex-shrink-0 rounded-xl shadow-md shadow-t-alt/40 justify-evenly flex-wrap">
-      <div className="flex gap-2 items-center">
-        <div className="relative h-10 w-10">
-          <Image src="/icon.png" alt="" layout="fill" />
-        </div>
-        <h4 className="font-fredoka caps-small text-3xl">Url Shorten</h4>
-      </div>
-      <ul className="list-disc">
-        <li>2022 - KDWeb v1.0 - Sous licence MIT</li>
-        <li>Pour signaler des potentiels bugs, rendez-vous <Link href="/"><a className="underline tracking-widest font-fredoka underline-offset-4">ici</a></Link></li>
-      </ul>
-    </footer>
-  );
+	return (
+		<footer className="w-full flex items-center bg-gradient-to-b from-black/10 to-black/30 p-4 flex-shrink-0 rounded-xl shadow-md shadow-t-alt/40 justify-evenly flex-wrap mt-4">
+			<div className="flex gap-2 items-center">
+				<Logo className="w-10 h-10" background="rgba(0,0,0,0.4)" />
+				<h4 className="font-fredoka caps-small text-3xl">Url Shorten</h4>
+			</div>
+			<ul className="list-disc">
+				<li>2022 - KDWeb v1.0 - Sous licence MIT</li>
+				<li>
+					Pour signaler des potentiels bugs, rendez-vous{" "}
+					<Link href="/">
+						<a className="underline tracking-widest font-fredoka underline-offset-4">
+							ici
+						</a>
+					</Link>
+				</li>
+			</ul>
+		</footer>
+	)
 }
 
-export default Footer;
+export default Footer
--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,17 +1,24 @@
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
+import { LogoColors } from "./svg"
 
 const Header = () => {
-  return (
-    <header className="">
-      <Link href="/">LOGO - Home</Link>
+	return (
+		<header className="w-full flex items-center bg-gradient-to-b from-black/10 to-black/30 p-4 flex-shrink-0 rounded-xl shadow-md shadow-t-alt/40 justify-between flex-wrap mb-4">
+			<Link href="/">
+				<a className="flex flex-wrap items-center gap-2">
+					<LogoColors className="h-10 w-10" />
+					<h4 className="font-fredoka caps-small text-3xl">Url Shorten</h4>
+				</a>
+			</Link>
 
-      <nav>
-        <Link href="/">Create Url</Link>
-        <Link href="/">My Urls</Link>
-        <Link href="/">Account</Link>
-      </nav>
-    </header>
-  );
+			<nav>
+				<Link href="/">Create Url</Link>
+				<Link href="/">My Urls</Link>
+				<Link href="/">Account</Link>
+			</nav>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
