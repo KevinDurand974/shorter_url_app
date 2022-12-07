4c29933923a3310e64c102abef81dbc3c9524340
Kdurand974 <k.durand.974@gmail.com>
2022-12-07 20:16:26 +0100

    Update Footer

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="w-full flex items-center bg-gradient-to-b from-black/10 to-black/30 p-4 flex-shrink-0 rounded-xl shadow-md shadow-t-alt/40 justify-evenly flex-wrap">
       <div className="flex gap-2 items-center">
-        {/* FIX: Image */}
         <div className="relative h-10 w-10">
           <Image src="/icon.png" alt="" layout="fill" />
         </div>
@@ -12,7 +12,7 @@ const Footer = () => {
       </div>
       <ul className="list-disc">
         <li>2022 - KDWeb v1.0 - Sous licence MIT</li>
-        <li>Pour signaler des potentiels bugs, rendez-vous ici</li>
+        <li>Pour signaler des potentiels bugs, rendez-vous <Link href="/"><a className="underline tracking-widest font-fredoka underline-offset-4">ici</a></Link></li>
       </ul>
     </footer>
   );
