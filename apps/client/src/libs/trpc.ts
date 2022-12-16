06db0ceecc2919cf7f239330912b37c5ed8a06cc
Kdurand974 <k.durand.974@gmail.com>
2022-12-16 21:39:49 +0100

    Update trpc proxy with new header

--- apps/client/src/libs/trpc.ts
+++ apps/client/src/libs/trpc.ts
@@ -1,16 +1,27 @@
-import { httpBatchLink, createTRPCProxyClient } from "@trpc/client"
+import { httpBatchLink, createTRPCProxyClient, loggerLink } from "@trpc/client"
 
 import { AppRouter } from "@shorter/backend"
+
+let authToken: string
+
+const setAuthToken = (token: string) => {
+	authToken = token
+}
 
 const trpc = createTRPCProxyClient<AppRouter>({
 	links: [
 		httpBatchLink({
 			url: "http://localhost:8080/trpc",
-			fetch(url, options) {
+			fetch: async (url, options) => {
 				return fetch(url, { ...options, credentials: "include" })
+			},
+			headers: () => {
+				return {
+					Authorization: `Bearer ${authToken}`,
+				}
 			},
 		}),
 	],
 })
 
-export { trpc }
+export { trpc, setAuthToken }
