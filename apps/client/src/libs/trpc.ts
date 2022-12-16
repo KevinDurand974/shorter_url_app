3cac1cb4624eb56378590ef065e20ffaf6e013ae
Kdurand974 <k.durand.974@gmail.com>
2022-12-16 17:11:09 +0100

    Activate auth credentials

--- apps/client/src/libs/trpc.ts
+++ apps/client/src/libs/trpc.ts
@@ -1,13 +1,16 @@
-import { httpBatchLink, createTRPCProxyClient } from "@trpc/client";
+import { httpBatchLink, createTRPCProxyClient } from "@trpc/client"
 
-import { AppRouter } from "@shorter/backend";
+import { AppRouter } from "@shorter/backend"
 
 const trpc = createTRPCProxyClient<AppRouter>({
 	links: [
 		httpBatchLink({
 			url: "http://localhost:8080/trpc",
+			fetch(url, options) {
+				return fetch(url, { ...options, credentials: "include" })
+			},
 		}),
 	],
-});
+})
 
-export { trpc };
+export { trpc }
