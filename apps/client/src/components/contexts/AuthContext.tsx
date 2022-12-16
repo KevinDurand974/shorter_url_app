f6d0621ad56ccf2726c2f080e4fa9f7fb5a68dea
Kdurand974 <k.durand.974@gmail.com>
2022-12-16 21:41:59 +0100

    Add AuthContext and set to all page

---
+++ apps/client/src/components/contexts/AuthContext.tsx
@@ -0,0 +1,57 @@
+import useSessionStorage from "@hooks/useSessionStorage"
+import { decode } from "jsonwebtoken"
+import { createContext, PropsWithChildren, useState } from "react"
+
+type ProviderProps = PropsWithChildren<{}>
+
+export type AuthUser = {
+	uuid: string
+	pseudo: string
+	vip: boolean
+	emailVerified: boolean
+}
+
+const AuthContext = createContext<{
+	isLogged: boolean
+	getUser: () => AuthUser | null
+	setUser: (u: AuthUser) => void
+}>({
+	isLogged: false,
+	getUser: () => null,
+	setUser: (u: AuthUser) => {},
+})
+
+export const AuthProvider = ({ children }: ProviderProps) => {
+	const [isLogged, setLogged] = useState(false)
+	const [user, setUser] = useState<AuthUser | null>(null)
+
+	const { isSessionAvailable, getSessionValue } = useSessionStorage()
+
+	if (isSessionAvailable()) {
+		if (!user) {
+			const token = getSessionValue("us_at")
+			if (token) {
+				const userInfo = decode(token) as AuthUser
+				setUser(userInfo)
+				setLogged(true)
+			}
+		}
+	}
+
+	const updateUserInfo = (user: AuthUser) => {
+		setUser(user)
+	}
+	const getUserInfo = () => {
+		return user
+	}
+
+	const values = {
+		isLogged,
+		getUser: getUserInfo,
+		setUser: updateUserInfo,
+	}
+
+	return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+}
+
+export default AuthContext
