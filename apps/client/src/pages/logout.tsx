0f3974e8042c9ac176a7aee9409cc7bb12065733
Kdurand974 <k.durand.974@gmail.com>
2022-12-17 14:38:15 +0100

    Update fonctionnality on logout

--- apps/client/src/pages/logout.tsx
+++ apps/client/src/pages/logout.tsx
@@ -1,5 +1,36 @@
+import { AuthContext } from "@contexts"
+import useHasMounted from "@hooks/useHasMounted"
+import useSessionStorage from "@hooks/useSessionStorage"
+import { trpc } from "@libs/trpc"
+import { useRouter } from "next/router"
+import { useContext } from "react"
+
 const LogoutPage = () => {
-	return <h1>Cannot be here bruh</h1>
+	const { logout, isLogged } = useContext(AuthContext)
+	const { push } = useRouter()
+	const hasMounted = useHasMounted()
+	const { isSessionAvailable, removeSessionValue } = useSessionStorage()
+
+	if (hasMounted && isLogged) {
+		;(async () => {
+			try {
+				const ac = new AbortController()
+				await trpc.logout.mutate(undefined, { signal: ac.signal })
+			} catch (err: any) {
+				console.error(err.message)
+			} finally {
+				if (isSessionAvailable()) removeSessionValue("us_at")
+				logout()
+				push("/")
+			}
+		})()
+	}
+
+	if (hasMounted && !isLogged) {
+		push("/")
+	}
+
+	return null
 }
 
 export default LogoutPage
