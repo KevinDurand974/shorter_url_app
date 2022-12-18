371f907b319e14a2ea9e89bbd2f03ff6e996b2a6
Kdurand974 <k.durand.974@gmail.com>
2022-12-18 12:07:18 +0100

    Add rememberme to zod loginSchema

--- libs/validators/src/auth.ts
+++ libs/validators/src/auth.ts
@@ -1,8 +1,9 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const loginSchema = z.object({
 	email: z.string().email(),
 	password: z.string(),
-});
+	rememberme: z.boolean(),
+})
 
-export type LoginSchema = z.infer<typeof loginSchema>;
+export type LoginSchema = z.infer<typeof loginSchema>
