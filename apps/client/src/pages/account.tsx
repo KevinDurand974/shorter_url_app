d9e56de478b32ae9c5bde079639c821b3e47b237
Kdurand974 <k.durand.974@gmail.com>
2022-12-19 15:17:56 +0100

    Add some boiler plate for test

--- apps/client/src/pages/account.tsx
+++ apps/client/src/pages/account.tsx
@@ -1,4 +1,30 @@
+import { AuthContext } from "@contexts"
+import { trpc } from "@libs/trpc"
+import { useContext, useEffect } from "react"
+
 const AccountSettingsPage = () => {
+	const { user, isLogged } = useContext(AuthContext)
+
+	useEffect(() => {
+		const run = async () => {
+			try {
+				if (!isLogged) return
+				const ac = new AbortController()
+				const userData = await trpc.getUser.query(
+					{ uuid: user!.uuid },
+					{
+						signal: ac.signal,
+					}
+				)
+
+				console.log(userData)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+		run()
+	}, [isLogged, user])
+
 	return <h1>Account</h1>
 }
 
--- apps/client/src/pages/explorer.tsx
+++ apps/client/src/pages/explorer.tsx
@@ -1,4 +1,26 @@
+import useHasMounted from "@hooks/useHasMounted"
+import { trpc } from "@libs/trpc"
+import { useEffect, useState } from "react"
+
 const ExplorerUrlPage = () => {
+	const [data, setData] = useState<any>(null)
+
+	useEffect(() => {
+		const run = async () => {
+			try {
+				const ac = new AbortController()
+				const dataUrls = await trpc.getUrls.query(undefined, {
+					signal: ac.signal,
+				})
+
+				console.log(dataUrls)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+		run()
+	}, [])
+
 	return <h1>My Urls</h1>
 }
 
