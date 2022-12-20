772b6ac8c25e1b10ab47032b7fc8a18dd5ebc1d1
Kdurand974 <k.durand.974@gmail.com>
2022-12-20 17:05:54 +0100

    Update login validator with cookie condition

--- libs/validators/src/auth.ts
+++ libs/validators/src/auth.ts
@@ -4,6 +4,7 @@ export const loginSchema = z.object({
 	email: z.string().email(),
 	password: z.string(),
 	rememberme: z.boolean(),
+	enabledCookie: z.boolean(),
 })
 
 export type LoginSchema = z.infer<typeof loginSchema>
