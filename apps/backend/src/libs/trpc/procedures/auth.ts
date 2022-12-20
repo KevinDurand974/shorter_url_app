06cbb26bf15ec3f8a1b714fceff364c63e635757
Kdurand974 <k.durand.974@gmail.com>
2022-12-20 17:07:54 +0100

    Remove refresh route, and change the return of login route

--- apps/backend/src/libs/trpc/procedures/auth.ts
+++ apps/backend/src/libs/trpc/procedures/auth.ts
@@ -1,13 +1,13 @@
 import { createUserSchema, loginSchema } from '@shorter/validators';
 import { login, logout, refreshToken, register } from '../../../models';
 import { getDataSource } from '../../typeorm';
-import { publicProcedure, refreshProcedure, router } from '../configuration';
+import { publicProcedure, router } from '../configuration';
 
 export const authRouter = router({
   // NOTE: Public Procedures
   login: publicProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
     const datasource = await getDataSource();
-    return login(datasource, input, ctx);
+    await login(datasource, input, ctx);
   }),
   register: publicProcedure.input(createUserSchema).mutation(async ({ input, ctx }) => {
     const datasource = await getDataSource();
@@ -17,12 +17,6 @@ export const authRouter = router({
     const datasource = await getDataSource();
     await logout(datasource, ctx);
   }),
-  refresh: publicProcedure.query(async ({ ctx }) => {
-    const datasource = await getDataSource();
-    return refreshToken(datasource, ctx);
-  }),
-  // NOTE: Refresh Procedures
-  verifyToken: refreshProcedure.query(({ ctx }) => ctx.tokenValid),
 
   // TODO: Forgot Password ?
 });
