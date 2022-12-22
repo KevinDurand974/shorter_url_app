d1f0d94acca252ecff9c2bb8e582f5b3d19d1269
Kdurand974 <k.durand.974@gmail.com>
2022-12-22 18:00:18 +0100

    Add optionnal to enabledCookie property in auth login zod schema

--- libs/validators/src/auth.ts
+++ libs/validators/src/auth.ts
@@ -4,7 +4,7 @@ export const loginSchema = z.object({
 	email: z.string().email(),
 	password: z.string(),
 	rememberme: z.boolean(),
-	enabledCookie: z.boolean(),
+	enabledCookie: z.boolean().optional(),
 })
 
 export type LoginSchema = z.infer<typeof loginSchema>
