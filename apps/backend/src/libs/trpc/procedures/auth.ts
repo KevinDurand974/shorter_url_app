ee08cf2272c122e39197f0c7b10027ae0fdeaa7b
Kdurand974 <k.durand.974@gmail.com>
2022-12-22 17:57:26 +0100

    Add testAuth to check user auth

--- apps/backend/src/libs/trpc/procedures/auth.ts
+++ apps/backend/src/libs/trpc/procedures/auth.ts
@@ -1,13 +1,13 @@
 import { createUserSchema, loginSchema } from '@shorter/validators';
 import { login, logout, refreshToken, register } from '../../../models';
 import { getDataSource } from '../../typeorm';
-import { publicProcedure, router } from '../configuration';
+import { authProcedure, publicProcedure, router } from '../configuration';
 
 export const authRouter = router({
   // NOTE: Public Procedures
   login: publicProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
     const datasource = await getDataSource();
-    await login(datasource, input, ctx);
+    return login(datasource, input, ctx);
   }),
   register: publicProcedure.input(createUserSchema).mutation(async ({ input, ctx }) => {
     const datasource = await getDataSource();
@@ -17,6 +17,7 @@ export const authRouter = router({
     const datasource = await getDataSource();
     await logout(datasource, ctx);
   }),
+  testAuth: authProcedure.query(async ({ ctx }) => ({ valid: true })),
 
   // TODO: Forgot Password ?
 });
