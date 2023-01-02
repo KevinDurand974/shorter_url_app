a27564015bdf4699be96219ad19780193d86c7d2
Kdurand974 <k.durand.974@gmail.com>
2023-01-02 19:06:20 +0100

    Divide Zod Schema in one pure and one with refinement

--- libs/validators/src/user.ts
+++ libs/validators/src/user.ts
@@ -2,33 +2,33 @@ import { z } from "zod"
 import isStrongPassword from "validator/lib/isStrongPassword"
 
 // Schemas
-export const createUserSchema = z
-	.object({
-		email: z.string().max(254).email(),
-		password: z
-			.string()
-			.min(8)
-			.max(60)
-			.refine(
-				isStrongPassword,
-				"Must contain 1 upper case character, 1 lower case character and 1 symbol."
-			),
-		password2: z
-			.string()
-			.min(8)
-			.max(60)
-			.refine(
-				isStrongPassword,
-				"Must contain 1 upper case character, 1 lower case character and 1 symbol."
-			),
-		pseudo: z.string().min(3).max(20),
-		urlname: z
-			.string()
-			.min(3)
-			.max(20)
-			.regex(/^[a-z0-9]+$/, "Only lowercase letters and numbers are allowed"),
-	})
-	.superRefine(({ password, password2 }, ctx) => {
+export const createUserDefaultSchema = z.object({
+	email: z.string().max(254).email(),
+	password: z
+		.string()
+		.min(8)
+		.max(60)
+		.refine(
+			isStrongPassword,
+			"Must contain 1 upper case character, 1 lower case character and 1 symbol."
+		),
+	password2: z
+		.string()
+		.min(8)
+		.max(60)
+		.refine(
+			isStrongPassword,
+			"Must contain 1 upper case character, 1 lower case character and 1 symbol."
+		),
+	pseudo: z.string().min(3).max(20),
+	urlname: z
+		.string()
+		.min(3)
+		.max(20)
+		.regex(/^[a-z0-9]+$/, "Only lowercase letters and numbers are allowed"),
+})
+export const createUserSchema = createUserDefaultSchema.superRefine(
+	({ password, password2 }, ctx) => {
 		if (password !== password2) {
 			ctx.addIssue({
 				code: "custom",
@@ -36,10 +36,19 @@ export const createUserSchema = z
 				path: ["password2"],
 			})
 		}
-	})
+	}
+)
 export const updateUserEmailDefaultSchema = z.object({
-	currentEmail: z.string().max(254).email(),
-	newEmail: z.string().max(254).email(),
+	currentEmail: z
+		.string()
+		.min(1, "Cannot be empty.")
+		.max(254, "Email characters are limited to 254.")
+		.email("Email not valid."),
+	newEmail: z
+		.string()
+		.min(1, "Cannot be empty.")
+		.max(254, "Email characters are limited to 254.")
+		.email("Email not valid."),
 })
 export const updateUserEmailSchema = updateUserEmailDefaultSchema.superRefine(
 	({ currentEmail, newEmail }, ctx) => {
@@ -75,8 +84,12 @@ export const deleteUserSchema = z.object({
 export const getUserSchema = z.object({
 	uuid: z.string().uuid(),
 })
+export const checkEmailSchema = z.object({
+	email: z.string().email(),
+})
 
 // Types
+export type CreateUserDefaultSchema = z.infer<typeof createUserDefaultSchema>
 export type CreateUserSchema = z.infer<typeof createUserSchema>
 export type UpdateUserEmailSchema = z.infer<typeof updateUserEmailDefaultSchema>
 export type UpdateUserPasswordSchema = z.infer<typeof updateUserPasswordSchema>
@@ -85,3 +98,4 @@ export type UpdateUserVIPSchema = z.infer<typeof updateUserVIPSchema>
 export type UpdateUserPseudoSchema = z.infer<typeof updateUserPseudoSchema>
 export type DeleteUserSchema = z.infer<typeof deleteUserSchema>
 export type GetUserSchema = z.infer<typeof getUserSchema>
+export type CheckEmailSchema = z.infer<typeof checkEmailSchema>
