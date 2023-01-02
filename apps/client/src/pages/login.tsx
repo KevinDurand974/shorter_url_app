c4abf16b072be51529a2fd6ae29c6fc2198190c2
Kdurand974 <k.durand.974@gmail.com>
2023-01-02 19:03:59 +0100

    Update with new form lib these pages

--- apps/client/src/pages/login.tsx
+++ apps/client/src/pages/login.tsx
@@ -1,20 +1,26 @@
 import { AuthContext } from "@contexts"
 import useLocalStorage from "@hooks/useLocalStorage"
+import {
+	Button,
+	ErrorMessage,
+	Field,
+	FormController,
+	FormValues,
+} from "@libs/Form"
 import { trpc } from "@libs/trpc"
 import { isAuthServer } from "@libs/trpcSsr"
-import { useFormikZodAdapter } from "@libs/useFormikZodAdapter"
 import { loginSchema } from "@shorter/validators"
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik"
 import { GetServerSideProps } from "next"
 import Head from "next/head"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { Fragment, useContext, useEffect, useState } from "react"
 
-type FormValues = {
+type FormatedValues = {
 	email: string
 	password: string
 	rememberme: boolean
+	enabledCookie: boolean
 }
 
 const LoginPage = () => {
@@ -27,21 +33,16 @@ const LoginPage = () => {
 		setEnabledCookie(navigator.cookieEnabled)
 	}, [])
 
-	const onSubmit = async (
-		values: FormValues,
-		{ setSubmitting, resetForm }: FormikHelpers<FormValues>
-	) => {
-		setSubmitting(false)
+	const onSubmit = async (values: FormValues) => {
 		try {
 			const ac = new AbortController()
 			const data = await trpc.login.mutate(
-				{ ...values, enabledCookie },
+				{ ...values, enabledCookie } as FormatedValues,
 				{ signal: ac.signal }
 			)
 			if (isStorageAvailable()) setStorageValue("logged_in", 1)
 			setUser(data.user)
 			// FIX: Implement no cookie here
-			resetForm()
 			await router.push("/")
 		} catch (err: any) {
 			// FIX: Use a toast to show the error
@@ -67,82 +68,74 @@ const LoginPage = () => {
 						Log in to your account
 					</h1>
 
-					<Formik
-						// FIX: Leave only rememberme to true
+					<FormController
 						initialValues={{
+							// FIX: Leave only rememberme to true
 							email: "test1@test.fr",
 							password: "Azerty&1",
 							rememberme: true,
 						}}
-						validationSchema={useFormikZodAdapter(loginSchema)}
+						schema={loginSchema}
 						onSubmit={onSubmit}
+						className="sm:box sm:p-4 flex flex-col gap-4"
 					>
-						{({ isSubmitting, isValid }) => (
-							<Form className="sm:box sm:p-4 flex flex-col gap-4">
-								<>
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="email"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Email
-										</label>
-										<Field
-											id="email"
-											type="email"
-											name="email"
-											placeholder="exemple@gmail.com"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="email"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="email"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Email
+							</label>
+							<Field
+								id="email"
+								type="email"
+								name="email"
+								placeholder="exemple@gmail.com"
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="email"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="password"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Password
-										</label>
-										<Field
-											id="password"
-											type="password"
-											name="password"
-											placeholder="password"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="password"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="password"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Password
+							</label>
+							<Field
+								id="password"
+								type="password"
+								name="password"
+								placeholder="password"
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="password"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<div className="flex gap-2">
-										<Field
-											id="rememberme"
-											type="checkbox"
-											name="rememberme"
-											className="cursor-pointer"
-										/>
-										<label htmlFor="rememberme">Keep me logged</label>
-									</div>
+						<div className="flex gap-2">
+							<Field
+								id="rememberme"
+								type="checkbox"
+								name="rememberme"
+								className="cursor-pointer"
+							/>
+							<label htmlFor="rememberme">Keep me logged</label>
+						</div>
 
-									<button
-										type="submit"
-										className="px-5 py-3 font-fredoka tracking-wider bg-gradient-to-r from-gradient-top to-accent mt-4 uppercase transition-all duration-[0.4s] text-sm sm:text-lg disabled:grayscale disabled:opacity-20"
-										disabled={!isValid || isSubmitting}
-									>
-										Connect me!
-									</button>
-								</>
-							</Form>
-						)}
-					</Formik>
+						<Button
+							type="submit"
+							className="px-5 py-3 font-fredoka tracking-wider bg-gradient-to-r from-gradient-top to-accent mt-4 uppercase transition-all duration-[0.4s] text-sm sm:text-lg disabled:grayscale disabled:opacity-20"
+						>
+							Connect me!
+						</Button>
+					</FormController>
 
 					<div className="flex items-center gap-2 justify-end">
 						<div>Doesn&apos;t have an account?</div>
--- apps/client/src/pages/register.tsx
+++ apps/client/src/pages/register.tsx
@@ -1,17 +1,20 @@
+import {
+	Button,
+	ErrorMessage,
+	Field,
+	FormController,
+	FormValues,
+} from "@libs/Form"
 import { trpc } from "@libs/trpc"
 import { isAuthServer } from "@libs/trpcSsr"
-import { useFormikZodAdapter } from "@libs/useFormikZodAdapter"
-import { createUserSchema } from "@shorter/validators"
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik"
+import { createUserSchema, createUserDefaultSchema } from "@shorter/validators"
 import { GetServerSideProps } from "next"
 import Head from "next/head"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { Fragment } from "react"
 
-type Props = {}
-
-type FormValues = {
+type FormatedValues = {
 	email: string
 	password: string
 	password2: string
@@ -19,23 +22,30 @@ type FormValues = {
 	urlname: string
 }
 
-const RegisterPage = (props: Props) => {
+const RegisterPage = () => {
 	const router = useRouter()
 
-	const onSubmit = async (
-		values: FormValues,
-		{ setSubmitting, resetForm }: FormikHelpers<FormValues>
-	) => {
-		setSubmitting(false)
+	const onSubmit = async (values: FormValues) => {
 		try {
 			const ac = new AbortController()
-			await trpc.register.mutate(values, { signal: ac.signal })
-			resetForm()
+			await trpc.register.mutate(values as FormatedValues, {
+				signal: ac.signal,
+			})
+			// FIX - Make a reset form function
 			await router.push("/login")
 		} catch (err: any) {
 			// FIX: Use a toast to show the error
 			console.log(err.message)
 		}
+	}
+
+	// FIX: Leave only rememberme to true
+	const initialValues = {
+		email: "test2@test.fr",
+		password: "Azerty&1",
+		password2: "Azerty&1",
+		pseudo: "Test2",
+		urlname: "test2",
 	}
 
 	return (
@@ -53,141 +63,127 @@ const RegisterPage = (props: Props) => {
 						Create your account.
 					</h1>
 
-					<Formik
-						// FIX: Leave only rememberme to true
-						initialValues={{
-							email: "test2@test.fr",
-							password: "Azerty&1",
-							password2: "Azerty&1",
-							pseudo: "Test2",
-							urlname: "test2",
-						}}
-						validationSchema={useFormikZodAdapter(createUserSchema)}
-						validateOnMount={true}
+					<FormController
+						initialValues={initialValues}
+						schema={createUserSchema}
 						onSubmit={onSubmit}
+						className="sm:box sm:p-4 flex flex-col gap-4"
 					>
-						{({ isSubmitting, isValid, errors, values }) => (
-							<Form className="sm:box sm:p-4 flex flex-col gap-4">
-								<>
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="email"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Email
-										</label>
-										<Field
-											id="email"
-											type="email"
-											name="email"
-											placeholder="exemple@gmail.com"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="email"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="email"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Email
+							</label>
+							<Field
+								id="email"
+								type="email"
+								name="email"
+								placeholder="exemple@gmail.com"
+								schema={createUserDefaultSchema.shape.email}
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="email"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="password"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Password
-										</label>
-										<Field
-											id="password"
-											type="password"
-											name="password"
-											placeholder="password"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="password"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="password"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Password
+							</label>
+							<Field
+								id="password"
+								type="password"
+								name="password"
+								placeholder="password"
+								schema={createUserDefaultSchema.shape.password}
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="password"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="password"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Password Confirmation
-										</label>
-										<Field
-											id="password2"
-											type="password"
-											name="password2"
-											placeholder="password confirmation"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="password2"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="password"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Password Confirmation
+							</label>
+							<Field
+								id="password2"
+								type="password"
+								name="password2"
+								placeholder="password confirmation"
+								schema={createUserDefaultSchema.shape.password2}
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="password2"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="Pseudo"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Pseudo{" "}
-											<span className="text-xs caps-normal text-muted">
-												Will be only display to you
-											</span>
-										</label>
-										<Field
-											id="pseudo"
-											type="text"
-											name="pseudo"
-											placeholder="Pseudo"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="pseudo"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="Pseudo"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Pseudo{" "}
+								<span className="text-xs caps-normal text-muted">
+									Will be only display to you
+								</span>
+							</label>
+							<Field
+								id="pseudo"
+								type="text"
+								name="pseudo"
+								placeholder="Pseudo"
+								schema={createUserDefaultSchema.shape.pseudo}
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="pseudo"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<div className="flex flex-col gap-4">
-										<label
-											htmlFor="urlname"
-											className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
-										>
-											&gt; Personnal URL name
-										</label>
-										<Field
-											id="urlname"
-											type="text"
-											name="urlname"
-											placeholder="Url Name"
-											className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
-										/>
-										<ErrorMessage
-											name="urlname"
-											component="div"
-											className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
-										/>
-									</div>
+						<div className="flex flex-col gap-4">
+							<label
+								htmlFor="urlname"
+								className="custom-underline before:right-0 before:bottom-[-2px] w-fit font-bold caps-small text-lg tracking-wider pr-4"
+							>
+								&gt; Personnal URL name
+							</label>
+							<Field
+								id="urlname"
+								type="text"
+								name="urlname"
+								placeholder="Url Name"
+								schema={createUserDefaultSchema.shape.urlname}
+								className="bg-t-alt/60 border-2 sm:border-4 px-5 py-3 font-fredoka tracking-wider  placeholder:text-text/20 shadow-lg shadow-transparent hover:shadow-accent/50 transition-all duration-[0.4s] outline-none focus:shadow-gradient-top text-sm sm:text-base border-image"
+							/>
+							<ErrorMessage
+								name="urlname"
+								className="font-fredoka tracking-wider text-sm -mt-2 text-danger/80"
+							/>
+						</div>
 
-									<button
-										type="submit"
-										className="px-5 py-3 font-fredoka tracking-wider bg-gradient-to-r from-gradient-top to-accent mt-4 uppercase transition-all duration-[0.4s] text-sm sm:text-lg disabled:grayscale disabled:opacity-20"
-										disabled={!isValid || isSubmitting}
-									>
-										Register my account!
-									</button>
-								</>
-							</Form>
-						)}
-					</Formik>
+						<Button
+							type="submit"
+							className="px-5 py-3 font-fredoka tracking-wider bg-gradient-to-r from-gradient-top to-accent mt-4 uppercase transition-all duration-[0.4s] text-sm sm:text-lg disabled:grayscale disabled:opacity-20"
+						>
+							Register my account!
+						</Button>
+					</FormController>
 
 					<div className="flex items-center gap-2 justify-end">
 						<div>Already have an account?</div>
