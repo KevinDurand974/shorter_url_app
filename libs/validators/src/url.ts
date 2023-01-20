e490618edca8025fed99de775debbaca1d6f776d
Kdurand974 <k.durand.974@gmail.com>
2023-01-20 16:36:29 +0100

    Update url validator

--- libs/validators/src/url.ts
+++ libs/validators/src/url.ts
@@ -1,5 +1,5 @@
-import isURL from "validator/lib/isURL";
-import { z } from "zod";
+import isURL from "validator/lib/isURL"
+import { z } from "zod"
 
 export const createUrlSchema = z.object({
 	useUrlName: z.boolean(),
@@ -12,20 +12,24 @@ export const createUrlSchema = z.object({
 		"Invalid URL, please check your url, fragments are not allowed and protocol is required"
 	),
 	ephemeral: z.boolean(),
-	custom: z.string().min(5).max(60).optional(),
+	custom: z
+		.string()
+		.min(5, "Must be contain at least 5 characters")
+		.max(60, "Can contain up to 60 characters")
+		.optional(),
 	duration: z
 		.number()
 		.int()
 		.positive("Duration must be a positive integer")
-		.min(3600, "Minimum duration is 1 hour")
-		.max(5184000, "Maximum duration is 60 days")
+		.min(1, "Minimum duration is 1 hour")
+		.max(1440, "Maximum duration is 60 days (1440 hours)")
 		.optional()
-		.default(86400),
-});
+		.default(24),
+})
 
 export const deleteUrlSchema = z.object({
 	urlUuid: z.string().uuid(),
-});
+})
 
 export const updateUrlSchema = z.object({
 	urlUuid: z.string().uuid(),
@@ -45,21 +49,21 @@ export const updateUrlSchema = z.object({
 		.max(5184000, "Maximum duration is 60 days")
 		.optional()
 		.default(86400),
-});
+})
 
 export const getUrlSchema = z.object({
 	urlUuid: z.string().uuid(),
-});
+})
 
 export const updateUrlActiveStatusSchema = z.object({
 	urlUuid: z.string().uuid(),
 	active: z.boolean(),
-});
+})
 
-export type CreateUrlSchema = z.infer<typeof createUrlSchema>;
-export type DeleteUrlSchema = z.infer<typeof deleteUrlSchema>;
-export type UpdateUrlSchema = z.infer<typeof updateUrlSchema>;
-export type GetUrlSchema = z.infer<typeof getUrlSchema>;
+export type CreateUrlSchema = z.infer<typeof createUrlSchema>
+export type DeleteUrlSchema = z.infer<typeof deleteUrlSchema>
+export type UpdateUrlSchema = z.infer<typeof updateUrlSchema>
+export type GetUrlSchema = z.infer<typeof getUrlSchema>
 export type UpdateUrlActiveStatusSchema = z.infer<
 	typeof updateUrlActiveStatusSchema
->;
+>
