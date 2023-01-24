ba29b8b2a59ae5b702289dc48d4296ac3a3a855b
Kdurand974 <k.durand.974@gmail.com>
2023-01-24 14:47:23 +0100

    Update auth validator

--- libs/validators/src/auth.ts
+++ libs/validators/src/auth.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const loginSchema = z.object({
-	email: z.string().email(),
+	email: z.string().email("Must be a valid email"),
 	password: z.string(),
 	rememberme: z.boolean(),
 	enabledCookie: z.boolean().optional(),
