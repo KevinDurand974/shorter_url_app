8e9593c596fa04bd7a351bb60cdc6542a0386eba
Kdurand974 <k.durand.974@gmail.com>
2023-01-31 15:43:11 +0100

    Update filtering methods

--- apps/client/src/pages/explorer.tsx
+++ apps/client/src/pages/explorer.tsx
@@ -9,7 +9,7 @@ import { Fragment, useEffect, useState } from "react"
 import { BiEdit } from "react-icons/bi"
 import { HiOutlineEye } from "react-icons/hi2"
 import { IoClose } from "react-icons/io5"
-import { GoLock } from "react-icons/go"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
 import { FiCopy } from "react-icons/fi"
 import {
 	TiArrowUnsorted,
@@ -17,6 +17,7 @@ import {
 	TiArrowSortedDown,
 } from "react-icons/ti"
 import RestrictedArea from "@components/RestrictedArea"
+import { CopyButton } from "@components"
 
 type ServerProps = GetServerSideProps<{
 	isLogged: boolean
@@ -57,6 +58,11 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 		})
 	}
 
+	const isExpired = (hours: number, createdAt: Date) => {
+		const expirationDate = addHours(new Date(createdAt), hours)
+		return isBefore(expirationDate, new Date())
+	}
+
 	const filterBy = (
 		data: Url[],
 		sortBy: SortBy,
@@ -82,7 +88,16 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 				})
 				break
 			case "expireIn":
-				sort = data.sort((curr, prev) => {
+				const lifetime: Url[] = []
+				const others: Url[] = []
+				data.forEach((url) => {
+					if (url.ephemeral) {
+						others.push(url)
+					} else {
+						lifetime.push(url)
+					}
+				})
+				const otherSorted = others.sort((curr, prev) => {
 					const currParsed = addHours(new Date(curr.createdAt), curr.duration)
 					const prevParsed = addHours(new Date(prev.createdAt), prev.duration)
 
@@ -90,6 +105,7 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 
 					return filterByNumber(currParsed.getTime(), prevParsed.getTime())
 				})
+				sort = [...otherSorted, ...lifetime]
 				break
 			case "createdAt":
 				sort = data.sort((curr, prev) => {
@@ -127,11 +143,16 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 
 	const setFilterIcon = (name: SortBy) => {
 		if (getFilter === name) {
-			if (orderAsc === null) return <TiArrowUnsorted className="opacity-25" />
+			if (orderAsc === null)
+				return (
+					<TiArrowUnsorted className="opacity-10 group-hover:opacity-25 group:transition-all group:duration-300" />
+				)
 			if (orderAsc) return <TiArrowSortedDown />
 			if (!orderAsc) return <TiArrowSortedUp />
 		}
-		return <TiArrowUnsorted className="opacity-25" />
+		return (
+			<TiArrowUnsorted className="opacity-10 group-hover:opacity-25 group:transition-all group:duration-300" />
+		)
 	}
 
 	const handleSortChange = (name: SortBy) => {
@@ -141,7 +162,8 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 					setOrderAsc(false)
 					break
 				case false:
-					setOrderAsc(null)
+					setFilter("id")
+					setOrderAsc(true)
 					break
 				case null:
 					setOrderAsc(true)
@@ -173,7 +195,7 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 							<div className="table-cell p-2 border-r border-s-alt w-20">
 								<button
 									type="button"
-									className="flex gap-1 items-center justify-center w-full"
+									className="flex gap-1 items-center justify-center w-full group"
 									onClick={() => handleSortChange("id")}
 								>
 									<span>N°</span>
@@ -225,19 +247,34 @@ const ExplorerUrlPage = ({ isLogged, urls }: ClientProps) => {
 										kurl.app/{row.generatedUrl}
 									</div>
 									<div className="table-cell p-2 border-r border-s-alt align-middle text-center">
-										{createExpireIn(row.duration, row.createdAt)}
+										{row.ephemeral
+											? createExpireIn(row.duration, row.createdAt)
+											: "-"}
 									</div>
 									<div className="table-cell p-2 border-r border-s-alt align-middle text-center">
 										{format(new Date(row.createdAt), "P")}
 									</div>
 									<div className="table-cell p-2 text-center">
-										<button
+										{/* <button
 											type="button"
 											disabled={row.restricted}
 											className="text-3xl cta p-2 mr-2 disabled:grayscale disabled:pointer-events-none"
+											onClick={() =>
+												handleCopyLink(`kurl.app/${row.generatedUrl}`)
+											}
 										>
 											<FiCopy />
-										</button>
+										</button> */}
+										<CopyButton
+											copy={`kurl.app/${row.generatedUrl}`}
+											disabled={
+												row.restricted ||
+												(row.ephemeral &&
+													isExpired(row.duration, row.createdAt))
+											}
+										>
+											<FiCopy />
+										</CopyButton>
 										<button type="button" className="text-3xl cta p-2 mr-2">
 											<BiEdit />
 										</button>
